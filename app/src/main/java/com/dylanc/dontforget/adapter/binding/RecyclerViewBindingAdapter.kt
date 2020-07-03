ff6b9330c130de6ca49b5c052eec86a43d68acf9
dylanc <caishenglang@foxmail.com>
2020-07-03 17:55:20 +0800

    Fixed no list data in landscape screen

--- app/src/main/java/com/dylanc/dontforget/adapter/binding/RecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/dylanc/dontforget/adapter/binding/RecyclerViewBindingAdapter.kt
@@ -7,63 +7,69 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.drakeet.multitype.MultiTypeAdapter
 
-@BindingAdapter(value = ["adapter", "emptyView"])
-fun RecyclerView.setRecyclerAdapter(adapter: RecyclerView.Adapter<*>?, layoutId: Int = 0) {
+@Suppress("UNCHECKED_CAST")
+@BindingAdapter(value = ["adapter", "list", "emptyView"])
+fun <T : Any> RecyclerView.setRecyclerAdapter(
+  adapter: RecyclerView.Adapter<*>?,
+  list: List<T>?,
+  EmptyViewId: Int = 0
+) {
   if (adapter != null) {
     this.adapter = adapter
+    val emptyView: View? = findViewInLayout(EmptyViewId)
+    emptyView?.let { adapter.registerAdapterDataObserver(AdapterDataEmptyObserver(adapter, it)) }
   }
-  val emptyView: View? = findEmptyView(this, layoutId)
-  adapter?.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
-    override fun onChanged() {
-      super.onChanged()
-      checkEmpty()
-    }
 
-    override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-      super.onItemRangeInserted(positionStart, itemCount)
-      checkEmpty()
+  if (list != null) {
+    if (adapter is ListAdapter<*, *>) {
+      (adapter as ListAdapter<T, *>).submitList(list)
+    } else if (adapter is MultiTypeAdapter) {
+      adapter.items = list
+      adapter.notifyDataSetChanged()
     }
-
-    override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
-      super.onItemRangeRemoved(positionStart, itemCount)
-      checkEmpty()
-    }
-
-    private fun checkEmpty() {
-      if (adapter.itemCount == 0) {
-        emptyView?.visibility = View.VISIBLE
-      } else {
-        emptyView?.visibility = View.GONE
-      }
-    }
-  })
+  }
 }
 
-private fun findEmptyView(viewGroup: ViewGroup, layoutId: Int): View? {
-  val emptyView = viewGroup.findViewById<View>(layoutId)
+private fun ViewGroup.findViewInLayout(viewId: Int): View? {
+  val emptyView = findViewById<View>(viewId)
   return if (emptyView != null) {
     emptyView
   } else {
-    val parent = viewGroup.parent
+    val parent = parent
     if (parent != null && parent is ViewGroup) {
-      findEmptyView(parent, layoutId)
+      parent.findViewInLayout(viewId)
     } else {
       null
     }
   }
 }
 
-@Suppress("UNCHECKED_CAST")
-@BindingAdapter(value = ["list"])
-fun <T : Any> RecyclerView.refreshList(list: List<T>?) {
-  if (list == null) {
-    return
+private class AdapterDataEmptyObserver(
+  private val adapter: RecyclerView.Adapter<*>,
+  private val emptyView: View
+) : RecyclerView.AdapterDataObserver() {
+
+  override fun onChanged() {
+    super.onChanged()
+    checkEmpty()
   }
-  val adapter = this.adapter
-  if (adapter is ListAdapter<*, *>) {
-    (adapter as ListAdapter<T, *>).submitList(list)
-  } else if (adapter is MultiTypeAdapter) {
-    adapter.items = list
-    adapter.notifyDataSetChanged()
+
+  override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+    super.onItemRangeInserted(positionStart, itemCount)
+    checkEmpty()
   }
+
+  override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+    super.onItemRangeRemoved(positionStart, itemCount)
+    checkEmpty()
+  }
+
+  private fun checkEmpty() {
+    if (adapter.itemCount == 0) {
+      emptyView.visibility = View.VISIBLE
+    } else {
+      emptyView.visibility = View.GONE
+    }
+  }
+
 }
